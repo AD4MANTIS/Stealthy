5b89c6a125cf3bf5b152b92743db93199d25084f
AD4MANTIS <47518664+NIK-Viv@users.noreply.github.com>
2019-09-21 12:27:11 +0200

    Level + Audio Update

--- Stealthy/Assets/Scripts/SoundManager.cs
+++ Stealthy/Assets/Scripts/SoundManager.cs
@@ -1,4 +1,5 @@
 ﻿using nvp.events;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,24 +8,51 @@ public class SoundManager : MonoBehaviour
 {
     private AudioSource soundManager;
     public AudioClip chill;
+    public AudioClip death;
 
     private void OnEnable()
     {
         soundManager = GetComponent<AudioSource>();
-        soundManager.loop = true;
-        soundManager.clip = chill;
-        soundManager.Play();
-        NvpEventController.Events("PlayerSeesEnemy").GameEventHandler += SoundManager_GameEventHandler;
+
+        PlayOtherMusik(chill);
+        NvpEventController.Events("PlayerSeesEnemy").GameEventHandler += SoundManager_PlayerSeesEnemy;
+        NvpEventController.Events("EnemySeesPlayer").GameEventHandler += SoundManager_EnemySeesPlayer;
+        NvpEventController.Events("PlayerDies").GameEventHandler += SoundManager_PlayerDies;
+    }
+
+    private void SoundManager_PlayerDies(object sender, EventArgs e)
+    {
+        PlayOtherMusik(death);
     }
 
     private void OnDisable()
     {
-        NvpEventController.Events("PlayerSeesEnemy").GameEventHandler -= SoundManager_GameEventHandler;
+        NvpEventController.Events("PlayerSeesEnemy").GameEventHandler -= SoundManager_PlayerSeesEnemy;
+        NvpEventController.Events("EnemySeesPlayer").GameEventHandler -= SoundManager_EnemySeesPlayer;
+        NvpEventController.Events("PlayerDies").GameEventHandler -= SoundManager_PlayerDies;
+    }
+
+    private void SoundManager_EnemySeesPlayer(object sender, System.EventArgs e)
+    {
+        soundManager.Stop();
     }
 
 
-    private void SoundManager_GameEventHandler(object sender, System.EventArgs e)
+    private void SoundManager_PlayerSeesEnemy(object sender, System.EventArgs e)
     {
         soundManager.Stop();
+    }
+
+    private void PlayOtherMusik(AudioClip clip, float? delay = null)
+    {
+        if (clip is null)
+            return;
+
+        soundManager.Stop();
+        soundManager.clip = clip;
+        if (delay == null)
+            soundManager.Play();
+        else
+            soundManager.PlayDelayed((float)delay);
     }
 }
