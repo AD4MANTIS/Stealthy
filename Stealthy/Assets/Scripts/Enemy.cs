fde3bedf977507c3382890db86f75b022f88ce97
AD4MANTIS <47518664+NIK-Viv@users.noreply.github.com>
2019-09-21 10:05:01 +0200

    basic scripts

---
+++ Stealthy/Assets/Scripts/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using nvp.events;
+
+public class Enemy : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        NvpEventController.Events("OnHitByPlayer").TriggerEvent(this, null);
+    }
+
+    public void PointerEntered()
+    {
+    }
+}
