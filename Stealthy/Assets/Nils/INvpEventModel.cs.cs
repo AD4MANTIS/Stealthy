4eeafa05521fb2d6556faacb7322fc1b8b21a8d7
AD4MANTIS <47518664+NIK-Viv@users.noreply.github.com>
2019-09-21 10:04:12 +0200

    Event Handler

--- Stealthy/Assets/Nils/INvpEventModel.cs.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-public interface INvpEventModel
-{
-    void SubscribeToEvent(NvpGameEvents e, Action<object, object> callback);
-    void UnsubscribeFromEvent(NvpGameEvents e, Action<object, object> observer);
-    void InvokeEvent(NvpGameEvents e, object sender, object eventArgs);
-    void Reset();
-}
